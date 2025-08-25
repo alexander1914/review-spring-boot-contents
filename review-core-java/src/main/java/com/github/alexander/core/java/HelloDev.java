fde243942e97ba81f4bf732d40112201195ccd9e
alexander1914 <alexander.oliveira99@gmail.com>
2025-08-25 20:04:10 +0100

    [Core-Java] - I did set up the project will learn more about core java

---
+++ review-core-java/src/main/java/com/github/alexander/core/java/HelloDev.java
@@ -0,0 +1,8 @@
+package com.github.alexander.core.java;
+
+public class HelloDev {
+    public static void main(String[] args) {
+        String name = "Alexander Albuquerque Oliveira";
+        System.out.println("I'm a Principal Consultant in Europe: " + name);
+    }
+}
